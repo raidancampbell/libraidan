aa159aed16bf8e3e36a5bbe66f027f2ca040452a
R. Aidan Campbell <aidan.campbell@gmail.com>
2019-09-08 20:36:29 -0700

    initial commit

---
+++ pkg/rcollections/strMap.go
@@ -0,0 +1,271 @@
+package rcollections
+
+import (
+	"fmt"
+)
+
+type StrMap interface {
+	// Add adds the given key, value pair to the map.
+	// return indicates whether a value was overwritten
+	Add(key string, value interface{}) StrMap
+	// AddAll creates a new map of the combination of both.
+	// For intersections, the value from the other map is chosen
+	AddAll(other StrMap) StrMap
+	// Contains indicates whether the given key is inside the map
+	Contains(needle string) bool
+	// ContainsValue indicates whether the given value is inside the map
+	ContainsValue(needle interface{}) bool
+	// Get retrieves the value at the given key.  Returns a non-nil error If the value is not found
+	Get(needle string) (interface{}, error)
+	// GetWithDefault retrieves the value at the given key.
+	// If the key is found, the associated value is returned
+	// Otherwise, the given default value is returned
+	GetWithDefault(needle string, deflt interface{}) interface{}
+	// Remove removes the given key from the map
+	// If the key doesn't exist in the map, it's considered a no-op
+	Remove(key string) StrMap
+	// RemoveAll removes all the keys from the given keyslice from the map, and returns a new resulting map
+	// If the key doesn't exist in the map, it's considered a no-op
+	RemoveAll(keys []string) StrMap
+	// RemoveIntersections calls Remove for all keys in the other map, and returns a new resulting map
+	RemoveIntersections(other StrMap) StrMap
+	// Intersection is the resulting map of all keys in both maps.  The value is taken from the other map
+	Intersection(other StrMap) StrMap
+	// Disjunction is the resulting map of all keys NOT in both maps.
+	Disjunction(other StrMap) StrMap
+	// ContainsAll indicates whether this map contains all the keys of the other map
+	// i.e. is the set of this map's keys a superset of the other map's keys
+	// if other is empty, true is returned
+	ContainsAll(other StrMap) bool
+	// ContainsAny indicates whether this map contains any of the keys of the other map
+	// i.e. if ContainsAny returns true, the intersection would be non-empty
+	// if other is empty, true is returned
+	ContainsAny(other StrMap) bool
+	// Len returns the number of elements in the map
+	Len() int
+	// IsEmpty indicates whether the map is empty. i.e. its Len() is 0
+	IsEmpty() bool
+	// Equals indicates whether both maps have the same keysets, and each key's value is equal
+	Equals(other StrMap) bool
+	// ValueFrequency counts the number of times a value occurs in the map
+	ValueFrequency(needle interface{}) int
+	// Filter applies the given function to all values in the map.
+	// The function must receive a key:value keypair and return an indicator whether the element should be retained
+	// i.e. if the function returns true, it will appear in the resulting map
+	Filter(func(key string, val interface{}) bool) StrMap
+	// Map applies the given function to all values in the map.
+	// The function must receive a key:value keypair and return the mutated value
+	Map(func(key string, val interface{}) interface{}) StrMap
+	// Reduce applies the given function to all values in the map.
+	// The function must receive a key:value keypair along with an accumulator and return the mutated accumulator
+	// Since the accumulator is an interface, the function must handle an uninitialized accumulator and type it appropriately
+	Reduce(func(key string, val interface{}, acc interface{}) interface{}) interface{}
+	// innerMap is an accessor for this library
+	innerMap() map[string] interface{}
+}
+
+type strMap struct {
+	s map[string] interface{}
+}
+
+// AddAll overwrites the exiting keys
+func (s strMap) AddAll(other StrMap) StrMap {
+	newMap := NewStrMap()
+	
+	for k, v := range s.s {
+		newMap.innerMap()[k] = v
+	}
+	
+	for ok, ov := range other.innerMap() {
+		newMap.innerMap()[ok] = ov
+	}
+	return newMap
+}
+
+func NewStrMap() StrMap {
+	s := make(map[string] interface{})
+	return &strMap{s : s}
+}
+
+
+func (s strMap) Contains(needle string) bool {
+	for k := range s.s {
+		if needle == k {
+			return true
+		}
+	}
+	return false
+}
+
+func (s strMap) Add(key string, value interface{}) StrMap {
+	newStrMap := NewStrMap()
+	for k, v := range s.innerMap() {
+		newStrMap.innerMap()[k] = v
+	}
+	newStrMap.innerMap()[key] = value
+	return newStrMap
+}
+
+func (s strMap) Get(needle string) (interface{}, error) {
+	for k, v := range s.s {
+		if needle == k {
+			return v, nil
+		}
+	}
+	return nil, fmt.Errorf("key: %s not found", needle)
+}
+
+func (s strMap) GetWithDefault(needle string, deflt interface{}) interface{} {
+	val, err := s.Get(needle)
+	if err != nil {
+		return deflt
+	}
+	return val
+}
+
+func (s strMap) Remove(key string) StrMap {
+	newStrMap := NewStrMap()
+	for k, v := range s.innerMap() {
+		if k != key {
+			newStrMap.innerMap()[k] = v
+		}
+	}
+	return newStrMap
+}
+
+func (s strMap) RemoveAll(keys [] string) StrMap {
+	otherKeySet := NewStrMap()
+	for _, key := range keys {
+		otherKeySet.innerMap()[key] = nil
+	}
+	return s.RemoveIntersections(otherKeySet)
+}
+
+func (s strMap) ContainsValue(needle interface{}) bool {
+	for _, v := range s.s{
+		if v == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func (s strMap) ValueFrequency(needle interface{}) int {
+	freq := 0
+	for _, v := range s.s{
+		if v == needle {
+			freq++
+		}
+	}
+	return freq
+
+}
+
+func (s strMap) innerMap() map[string]interface{} {
+	return s.s
+}
+
+func (s strMap) Filter(f func(key string, val interface{}) bool) StrMap {
+	newMap := NewStrMap()
+	for k, v := range s.s {
+		shouldRetain := f(k, v)
+		if shouldRetain {
+			newMap.innerMap()[k] = v
+		}
+	}
+	return newMap
+}
+
+func (s strMap) Map(f func(key string, val interface{}) interface{}) StrMap {
+	newMap := NewStrMap()
+	for k, v := range s.s {
+		newMap.innerMap()[k] = f(k, v)
+	}
+	return newMap
+}
+
+func (s strMap) Reduce(f func(key string, val interface{}, acc interface{}) interface{}) interface{} {
+	var accumulator interface{}
+	for k, v := range s.s {
+		accumulator = f(k, v, accumulator)
+	}
+	return accumulator
+}
+
+func (s strMap) Intersection(other StrMap) StrMap {
+	newMap := NewStrMap()
+	for k, v := range other.innerMap() {
+		if s.Contains(k) {
+			newMap.innerMap()[k] = v
+		}
+	}
+	return newMap
+}
+
+func (s strMap) Disjunction(other StrMap) StrMap {
+	newMap := NewStrMap()
+	for k, v := range s.s {
+		if !other.Contains(k) {
+			newMap.innerMap()[k] = v
+		}
+	}
+
+	for k, v := range other.innerMap() {
+		if !s.Contains(k) {
+			newMap.innerMap()[k] = v
+		}
+	}
+
+	return newMap
+
+}
+
+func (s strMap) RemoveIntersections(other StrMap) StrMap {
+	newMap := NewStrMap()
+	for k, v := range s.s {
+		if !other.Contains(k) {
+			newMap.innerMap()[k] = v
+		}
+	}
+	return newMap
+}
+
+func (s strMap) ContainsAll(other StrMap) bool {
+	if other.IsEmpty() {
+		return true
+	}
+	for k := range other.innerMap() {
+		if !s.Contains(k) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s strMap) ContainsAny(other StrMap) bool {
+	for k := range other.innerMap() {
+		if s.Contains(k) {
+			return true
+		}
+	}
+	return other.IsEmpty()
+
+}
+
+func (s strMap) Len() int {
+	return len(s.innerMap())
+}
+
+func (s strMap) IsEmpty() bool {
+	return s.Len() == 0
+}
+
+// are these equal
+func (s strMap) Equals(other StrMap) bool {
+	equality := true
+	for k, v := range s.s {
+		ov, err := other.Get(k)
+		equality = equality && err == nil && v == ov
+	}
+	return equality
+}
---
+++ pkg/rcollections/strMap_test.go
@@ -0,0 +1,370 @@
+package rcollections
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestNewStrMap(t *testing.T) {
+	s := NewStrMap()
+	assert.NotNil(t, s)
+}
+
+func TestStrMap_Add(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("foo", "bar")
+	assert.Equal(t, s.innerMap()["foo"], "bar")
+}
+
+func TestStrMap_AddAll(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo key")
+	o = o.Add("barkey", "barval")
+
+	actual := s.AddAll(o)
+	assert.NotNil(t, actual)
+
+	val := actual.GetWithDefault("fookey", "")
+	assert.Equal(t, "other foo key", val)
+
+	val = actual.GetWithDefault("barkey", "")
+	assert.Equal(t, "barval", val)
+
+	val = actual.GetWithDefault("quxkey", "")
+	assert.Equal(t, "quxval", val)
+}
+
+func TestStrMap_Contains(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("foo", "bar")
+	assert.True(t, s.Contains("foo"))
+	assert.False(t, s.Contains("qux"))
+	assert.False(t, s.Contains(""))
+
+	s = s.Add("quux", nil)
+	assert.True(t, s.Contains("quux"))
+}
+
+func TestStrMap_ContainsValue(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("foo", "bar")
+	assert.True(t, s.ContainsValue("bar"))
+	assert.False(t, s.ContainsValue("qux"))
+	assert.False(t, s.ContainsValue(nil))
+
+	s = s.Add("quux", nil)
+	assert.True(t, s.ContainsValue(nil))
+}
+
+func TestStrMap_Get(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("foo", "bar")
+	actual, err := s.Get("foo")
+	assert.Equal(t, "bar", actual)
+	assert.Nil(t, err)
+
+	actual, err = s.Get("qux")
+	assert.Nil(t, actual)
+	assert.NotNil(t, err)
+}
+
+func TestStrMap_GetWithDefault(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("foo", "bar")
+	actual := s.GetWithDefault("foo", "default")
+	assert.Equal(t, "bar", actual)
+
+	actual = s.GetWithDefault("qux", "default")
+	assert.Equal(t, actual, "default")
+}
+
+func TestStrMap_Remove(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("foo", "bar")
+	a, err := s.Get("foo")
+	assert.Equal(t, "bar", a)
+	assert.Nil(t, err)
+
+	actual := s.Remove("foo")
+
+	a, err = actual.Get("foo")
+	assert.NotNil(t, err)
+	assert.Nil(t, a)
+}
+
+func TestStrMap_RemoveIntersections(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo key")
+	o = o.Add("barkey", "barval")
+
+	actual := s.RemoveIntersections(o)
+
+	a, err = actual.Get("fookey")
+	assert.NotNil(t, err)
+	assert.Nil(t, a)
+
+	a, err = actual.Get("barkey")
+	assert.NotNil(t, err)
+	assert.Nil(t, a)
+
+	a, err = actual.Get("quxkey")
+	assert.Nil(t, err)
+	assert.Equal(t, a, "quxval")
+
+}
+
+func TestStrMap_RemoveAll(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	actual := s.RemoveAll([]string{"fookey", "barkey"})
+
+	assert.Equal(t, actual.Len(), 1)
+	assert.False(t, actual.Contains("fookey"))
+	assert.True(t, actual.Contains("quxkey"))
+
+	actual = s.RemoveAll([]string{})
+
+	assert.True(t, s.Equals(actual))
+
+	actual = s.RemoveAll([]string{"fookey", "barkey", "quxkey"})
+	assert.True(t, actual.IsEmpty())
+}
+
+func TestStrMap_Intersection(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo val")
+	o = o.Add("barkey", "barval")
+
+	actual := s.Intersection(o)
+
+	a, err = actual.Get("fookey")
+	assert.Nil(t, err)
+	assert.Equal(t, a, "other foo val")
+
+	a, err = actual.Get("barkey")
+	assert.Nil(t, err)
+	assert.Equal(t, a, "barval")
+
+	a, err = actual.Get("quxkey")
+	assert.Nil(t, a)
+	assert.NotNil(t, err)
+}
+
+func TestStrMap_Disjunction(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo val")
+	o = o.Add("quxkey", "quxval")
+
+	actual := s.Disjunction(o)
+
+	a, err = actual.Get("fookey")
+	assert.Nil(t, a)
+	assert.NotNil(t, err)
+
+	a, err = actual.Get("barkey")
+	assert.Nil(t, err)
+	assert.Equal(t, a, "barval")
+
+	a, err = actual.Get("quxkey")
+	assert.Equal(t, a, "quxval")
+	assert.Nil(t, err)
+}
+
+func TestStrMap_ContainsAll(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo val")
+	o = o.Add("quxkey", "quxval")
+
+	actual := s.ContainsAll(o)
+	assert.False(t, actual)
+
+	s = s.Add("quxkey", "")
+	actual = s.ContainsAll(o)
+	assert.True(t, actual)
+
+	assert.True(t, NewStrMap().ContainsAll(NewStrMap()))
+	assert.True(t, s.ContainsAll(NewStrMap()))
+}
+
+func TestStrMap_ContainsAny(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo val")
+	o = o.Add("quxkey", "quxval")
+
+	actual := s.ContainsAny(o)
+	assert.True(t, actual)
+
+	s = s.Remove("fookey")
+	actual = s.ContainsAny(o)
+	assert.False(t, actual)
+
+	assert.True(t, NewStrMap().ContainsAny(NewStrMap()))
+	assert.True(t, s.ContainsAny(NewStrMap()))
+}
+
+func TestStrMap_Equals(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	o := NewStrMap()
+	o = o.Add("fookey", "other foo val")
+	o = o.Add("quxkey", "quxval")
+
+	assert.False(t, s.Equals(o))
+
+	o = o.Remove("quxkey")
+	o = o.Add( "barkey", "barval")
+	assert.False(t, s.Equals(o))
+
+	o = o.Add("fookey", "fooval")
+	assert.True(t, s.Equals(o))
+}
+
+func TestStrMap_ValueFrequency(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+	a, err := s.Get("fookey")
+	assert.Equal(t, "fooval", a)
+	assert.Nil(t, err)
+
+	assert.Equal(t, s.ValueFrequency("fooval"), 1)
+	assert.Equal(t, s.ValueFrequency("nonexistant"), 0)
+	s = s.Add("barkey", "fooval")
+	assert.Equal(t, s.ValueFrequency("fooval"), 2)
+}
+
+func TestStrMap_Filter(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+
+	f := func(_ string, val interface{}) bool {
+		s := val.(string)
+		suffix := strings.HasSuffix(s, "val")
+		return suffix
+	}
+
+	actual := s.Filter(f)
+	assert.True(t, s.Equals(actual))
+
+	s = s.Add("fookey", "val with different suffix")
+
+	actual = s.Filter(f)
+	assert.False(t, s.Equals(actual))
+	assert.False(t, actual.Contains("fookey"))
+}
+
+func TestStrMap_Map(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+
+	f := func(_ string, val interface{}) interface{} {
+		s := val.(string)
+		if strings.HasSuffix(s, "val") {
+			return s + "idated"
+		} else {
+			return s
+		}
+	}
+
+	actual := s.Map(f)
+	assert.False(t, s.Equals(actual))
+	assert.Equal(t, actual.GetWithDefault("fookey", ""), "foovalidated")
+	assert.Equal(t, actual.GetWithDefault("barkey", ""), "barvalidated")
+	assert.Equal(t, actual.GetWithDefault("quxkey", ""), "quxvalidated")
+
+	s = s.Add("fookey", "val with different suffix")
+	actual = s.Map(f)
+
+	assert.False(t, s.Equals(actual))
+	assert.Equal(t, actual.GetWithDefault("fookey", ""), "val with different suffix")
+	assert.Equal(t, actual.GetWithDefault("barkey", ""), "barvalidated")
+	assert.Equal(t, actual.GetWithDefault("quxkey", ""), "quxvalidated")
+}
+
+func TestStrMap_Reduce(t *testing.T) {
+	s := NewStrMap()
+	s = s.Add("fookey", "fooval")
+	s = s.Add("barkey", "barval")
+	s = s.Add("quxkey", "quxval")
+
+	f := func(key string, val, acc interface{}) interface{} {
+		valstr := val.(string)
+		var accstr string
+		if acc == nil {
+			accstr = ""
+		} else {
+			accstr = acc.(string)
+			accstr += ", "
+		}
+		return accstr + key + ":" + valstr
+	}
+
+	result := s.Reduce(f).(string)
+	assert.True(t, strings.Contains(result, "fookey:fooval"))
+	assert.True(t, strings.Contains(result, "barkey:barval"))
+	assert.True(t, strings.Contains(result, "quxkey:quxval"))
+
+	assert.Nil(t, NewStrMap().Reduce(f))
+}
