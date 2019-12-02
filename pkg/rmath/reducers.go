d0cdae7775a52caa8ab5d97b054fc71d516c09ce
R. Aidan Campbell <aidan.campbell@gmail.com>
2019-12-01 20:39:04 -0700

    add int min/max/sum functions

---
+++ pkg/rmath/reducers.go
@@ -0,0 +1,37 @@
+package rmath
+
+import "math"
+
+// returns the minimum of all passed ints.
+// if no ints are passed, then math.MaxInt64 is returned
+func Min(n ...int) (min int) {
+	min = math.MaxInt64
+	for _, val := range n {
+		if val < min {
+			min = val
+		}
+	}
+	return
+}
+
+// returns the maximum of all passed ints.
+// if no ints are passed, then math.MinInt64 is returned
+func Max(n ...int) (max int) {
+	max = math.MinInt64
+	for _, val := range n {
+		if val > max {
+			max = val
+		}
+	}
+	return
+}
+
+// returns the sum of all passed ints.
+// if no ints are passed, then 0 is returned
+func Sum(n ...int) (sum int) {
+	sum = 0
+	for _, val := range n {
+		sum += val
+	}
+	return
+}
---
+++ pkg/rmath/reducers_test.go
@@ -0,0 +1,75 @@
+package rmath
+
+import (
+	"math"
+	"testing"
+)
+
+func TestMax(t *testing.T) {
+	type args struct {
+		n []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantMax int
+	}{
+		{"happy", args{[]int{1,2,3}}, 3},
+		{"big", args{[]int{1,2,30000}}, 30000},
+		{"negative", args{[]int{-1,-2,-3}}, -1},
+		{"abuse", args{[]int{}}, math.MinInt64},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotMax := Max(tt.args.n...); gotMax != tt.wantMax {
+				t.Errorf("Max() = %v, want %v", gotMax, tt.wantMax)
+			}
+		})
+	}
+}
+
+func TestMin(t *testing.T) {
+	type args struct {
+		n []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantMin int
+	}{
+		{"happy", args{[]int{1,2,3}}, 1},
+		{"big", args{[]int{1,2,30000}}, 1},
+		{"negative", args{[]int{-1,-2,-3}}, -3},
+		{"abuse", args{[]int{}}, math.MaxInt64},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotMin := Min(tt.args.n...); gotMin != tt.wantMin {
+				t.Errorf("Min() = %v, want %v", gotMin, tt.wantMin)
+			}
+		})
+	}
+}
+
+func TestSum(t *testing.T) {
+	type args struct {
+		n []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantSum int
+	}{
+		{"happy", args{[]int{1,2,3}}, 6},
+		{"big", args{[]int{1,2,30000}}, 30003},
+		{"negative", args{[]int{-1,-2,-3}}, -6},
+		{"abuse", args{[]int{}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSum := Sum(tt.args.n...); gotSum != tt.wantSum {
+				t.Errorf("Sum() = %v, want %v", gotSum, tt.wantSum)
+			}
+		})
+	}
+}
