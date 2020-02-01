d0bdeb4ebd0e53f028b2c94a61d15bf43340d517
R. Aidan Campbell <aidan.campbell@gmail.com>
2020-02-01 12:20:13 -0700

    add CsvToMap function to read a CSV file and return the resulting map

--- pkg/rstrings/ioutils.go
+++ pkg/rstrings/ioutils.go
@@ -1,10 +1,12 @@
 package rstrings
 
 import (
+	"encoding/csv"
 	"io/ioutil"
 	"os"
 )
 
+// FileToString reads from the given filename and returns the contents as a string
 func FileToString(fname string) (result string, err error) {
 	file, err := os.Open(fname)
 	if err != nil {
@@ -19,5 +21,37 @@ func FileToString(fname string) (result string, err error) {
 	err = file.Close()
 
 	result = string(b)
+	return
+}
+
+// CsvToMap takes the given filename, reads the file in CSV format, and returns a 2d map
+// first dimension is the column name, second dimension is the row values for that column.
+// input CSV file is expected to have a header row.
+func CsvToMap(fname string) (result map[string] []string, err error) {
+	result = make(map[string] []string)
+
+	file, err := os.Open(fname)
+	if err != nil {
+		return
+	}
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	tmp := make(map[int] []string)
+	for i, record := range records { // for each row
+		if i == 0 {  // skip the header row
+			continue
+		} else {  // build the details indexed by column number
+			for colNum, entry := range record {
+				tmp[colNum] = append(tmp[colNum], entry)
+			}
+		}
+	}
+
+	// build the result map, marrying the column header and number
+	for colNum, header := range records[0] {
+		result[header] = tmp[colNum]
+	}
+
 	return
 }
