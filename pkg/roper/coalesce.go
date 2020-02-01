07abc51c01cb55680edabc840068da71e7f402ce
R. Aidan Campbell <aidan.campbell@gmail.com>
2020-02-01 10:05:12 -0700

    leverage IsDefault and variadic args for a generic coalescing function, First

--- pkg/roper/coalesce.go
+++ pkg/roper/coalesce.go
@@ -25,3 +25,44 @@ func ZeroOr(a, b int) int {
 	}
 	return b
 }
+
+// First is a coalescing function to return the first non-type-default argument in the variadic arg list
+// see IsDefaultValue for the default checking implementation
+func First (i ...interface{}) interface{}{
+	for _, v := range i {
+		if !IsDefaultValue(v) {
+			return v
+		}
+	}
+	return nil
+}
+// Coalesce is an alias for First
+var Coalesce = First
+
+// FirstStr is a string typed wrapper for First
+func FirstStr(s ...string) string {
+	// conversion is required, see https://golang.org/doc/faq#convert_slice_of_interface for details
+	i := make([]interface{}, len(s))
+	for idx, v := range s {
+		i[idx] = v
+	}
+	result := First(i...)
+	if result == nil {
+		return ""
+	}
+	return result.(string)
+}
+
+// FirstInt is an int typed wrapper for First
+func FirstInt(s ...int) int {
+	// conversion is required, see https://golang.org/doc/faq#convert_slice_of_interface for details
+	i := make([]interface{}, len(s))
+	for idx, v := range s {
+		i[idx] = v
+	}
+	result := First(i...)
+	if result == nil {
+		return 0
+	}
+	return result.(int)
+}
--- pkg/roper/coalesce_test.go
+++ pkg/roper/coalesce_test.go
@@ -31,3 +31,55 @@ func TestZeroOr(t *testing.T) {
 	actual = ZeroOr(nn, ni)
 	assert.Equal(t, -1, actual)
 }
+
+func TestFirstStr(t *testing.T) {
+	type args struct {
+		s []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"both non empty", args{[]string{"foo", "bar"}}, "foo"},
+		{"first empty", args{[]string{"", "bar"}}, "bar"},
+		{"second empty", args{[]string{"foo", ""}}, "foo"},
+		{"middle empty", args{[]string{"foo", "", "baz"}}, "foo"},
+		{"middle only", args{[]string{"", "foo", ""}}, "foo"},
+		{"all empty", args{[]string{"", "", ""}}, ""},
+		{"no args", args{[]string{}}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FirstStr(tt.args.s...); got != tt.want {
+				t.Errorf("FirstStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFirstInt(t *testing.T) {
+	type args struct {
+		s []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"both non empty", args{[]int{-1, 1}}, -1},
+		{"first empty", args{[]int{0, 1}}, 1},
+		{"second empty", args{[]int{-1, 0}}, -1},
+		{"middle empty", args{[]int{-1, 0, 1}}, -1},
+		{"middle only", args{[]int{0, 5, 0}}, 5},
+		{"all empty", args{[]int{0, 0, 0}}, 0},
+		{"no args", args{[]int{}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FirstInt(tt.args.s...); got != tt.want {
+				t.Errorf("FirstInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
